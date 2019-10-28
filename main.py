dda785d0d3c112e19aa07bad4285d8181a6ea895
James Rupa <jamesjrupa@gmail.com>
2019-10-28 13:23:57 -0400

    added user options

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import random
 
 print('this is a password generator')
-print('would u like to include symbols')
-print('would u like to use numbers')
-print('would u like to use capital letters')
+sym = input('would u like to include symbols: ').lower().replace(" ", "")
+num = input('would u like to use numbers: ').lower().replace(" ", "")
+cap = input('would u like to use capital letters: ').lower().replace(" ", "")
 len = int(input('enter length of password: '))
 
-# TODO: add logic if statements for different types of passwords
+pass_char = 'abcdefghijklmnopqrstuvwxyz'
 
-pass_char = 'abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?'
+if sym == 'yes':
+    pass_char += '!@#$%^&*()?'
+if num == 'yes':
+    pass_char += '01234567890'
+if cap == 'yes':
+    pass_char += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 password =  ''.join(random.sample(pass_char, len ))
 print(password)
