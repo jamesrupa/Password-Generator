9aa50d1c0e018eeffc8fe5a61daf8f91ed5405ac
James Rupa <jamesjrupa@gmail.com>
2019-10-30 13:18:23 -0400

    added md5 hashing

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 import random
+import hashlib
 
-print('This is a PASSWORD GENERATOR')
-sym = input('Would u like to include symbols: ').lower().replace(" ", "")
-num = input('Would u like to use numbers: ').lower().replace(" ", "")
-cap = input('Would u like to use capital letters: ').lower().replace(" ", "")
-len = int(input('Enter length of password: '))
+def menu():
+    print('''
+    This is a PASSWORD GENERATOR
+    ____________________________
+    1   -   Password Picker
+    2   -   MD5 Hasher
+    ''')
 
-pass_char = 'abcdefghijklmnopqrstuvwxyz'
+    usr_input = int(input('Menu choice: '))
+    if usr_input == 1:
+        password()
+    if usr_input == 2:
+        md5hash()
+    elif usr_input < 1 or usr_input > 2:
+        print('Invalid choice!')
+        menu()
 
-if sym == 'yes':
-    pass_char += '!@#$%^&*()?'
-if num == 'yes':
-    pass_char += '01234567890'
-if cap == 'yes':
-    pass_char += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def password():
+    sym = input('Would u like to include symbols: ').lower().replace(" ", "")
+    num = input('Would u like to use numbers: ').lower().replace(" ", "")
+    cap = input('Would u like to use capital letters: ').lower().replace(" ", "")
+    length = int(input('Enter length of password: '))
 
-password =  ''.join(random.sample(pass_char, len ))
-print(password)
+    pass_char = 'abcdefghijklmnopqrstuvwxyz'
+
+    if sym == 'yes':
+        pass_char += '!@#$%^&*()?'
+    if num == 'yes':
+        pass_char += '01234567890'
+    if cap == 'yes':
+        pass_char += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    password =  ''.join(random.sample(pass_char, length))
+    print (f'Your {length} digit password is: {password}')
+
+def md5hash():
+
+    pre_hash = input('Enter what you want to hash: ')
+    post_hash = hashlib.md5(pre_hash.encode())
+
+    print("Your MD5 hash is: ", end ="") 
+    print(post_hash.hexdigest()) 
+
+
+menu()
